b69170107bbac31b185d2c97896cef7bbefdd3fd
Jakob Kristensen <jakobk205@gmail.com>
2025-03-11 15:10:50 +0100

    Add Arduino setup and button input handling

--- main.py
+++ main.py
@@ -3,6 +3,16 @@ import pyfirmata
 import time
 
 
+## Setup for Arduino
+port = 'COM3'
+board = pyfirmata.Arduino(port)
+
+button = board.get_pin('d:2:i')
+
+it = pyfirmata.util.Iterator(board)
+it.start()
+
+## Setup for database
 connection = psycopg2.connect(database="score_system", user="postgres", password="password", host="127.0.0.1", port=5432)
 
 cursor = connection.cursor()
