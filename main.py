44044cf19a1200bff047984f68017e1305e7dbc5
MortenSkildpadd3 <at114758@edu.aarhustech.dk>
2025-03-11 15:08:12 +0100

    Remove unnecessary blank line in main.py

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import psycopg2
 import pyfirmata
 import time
 
-
 connection = psycopg2.connect(database="score_system", user="postgres", password="password", host="127.0.0.1", port=5432)
 
 cursor = connection.cursor()
