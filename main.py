bc3ac20b22d241f54bdc80d786508e169655d915
Jakob Kristensen <jakobk205@gmail.com>
2025-03-11 14:47:00 +0100

    Add initial project setup with database connection and data insertion

---
+++ main.py
@@ -0,0 +1,19 @@
+import psycopg2
+import pyfirmata
+import time
+
+
+connection = psycopg2.connect(database="score_system", user="postgres", password="password", host="127.0.0.1", port=5432)
+
+cursor = connection.cursor()
+
+cursor.execute("INSERT INTO public.score (tid, stilling, team) VALUES ('77', '240', 'team4');")
+
+connection.commit()
+
+cursor.execute("SELECT * FROM score;")
+record = cursor.fetchall()
+print("Data from Database:- ", record)
+print(len(record), "rows returned")
+
+connection.close()
