c0fca4e1f8c17e86740e285af399e6a922075299
Jakob Kristensen <jakobk205@gmail.com>
2025-03-13 10:29:07 +0100

    Refactor main.py to update database connection and add random score generation with timestamps; update requirements.txt for new dependencies

--- main.py
+++ main.py
@@ -1,26 +1,40 @@
 import psycopg2
 import pyfirmata
 import time
+from datetime import datetime
+import random
 
 ## Setup for Arduino
-port = 'COM3'
-board = pyfirmata.Arduino(port)
+#port = 'COM3'
+#board = pyfirmata.Arduino(port)
+#button = board.get_pin('d:2:i')
 
-button = board.get_pin('d:2:i')
-
-it = pyfirmata.util.Iterator(board)
-it.start()
+#it = pyfirmata.util.Iterator(board)
+#it.start()
 
 ## Setup for database
-connection = psycopg2.connect(database="score_system", user="postgres", password="password", host="127.0.0.1", port=5432)
+connection = psycopg2.connect(database="scoredb", user="postgres", password="postgres", host="127.0.0.1", port=5432)
+db = connection.cursor()
 
-cursor = connection.cursor()
+## Setup time
+startTime = datetime.now()
+print("Start running at: ", startTime)
 
-cursor.execute("INSERT INTO public.score (tid, stilling, team) VALUES ('77', '240', 'team4');")
+## Generate random data and time
 
-connection.commit()
+for i in range(10):
+    score = random.randint(1, 5)
+    timestamp = datetime.now()
 
-record = cursor.fetchall()
+    db.execute("INSERT INTO score_system.scoretb (time, score) VALUES (%s, %s)", (timestamp, score))
+    connection.commit()
+    
+    delay = random.randint(1, 5)
+    time.sleep(delay)
+
+## Read data from database
+db.execute("SELECT * FROM score_system.scoretb ORDER BY time DESC LIMIT 10")
+record = db.fetchall()
 print("Data from Database:- ", record)
 print(len(record), "rows returned")
 
